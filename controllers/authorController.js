5f7d82d6a2e2fc97505c95cdbe346f5f64853264
mika-alpha <yuukeikioku@gmail.com>
2023-02-21 18:20:48 -0500

    Add Author list view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,18 @@
 const Author = require('../models/author');
 
 exports.author_list = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author list');
+    Author.find()
+    .sort([["family_name", "ascending"]])
+    .exec(function (err, list_authors){
+        if (err){
+            next(err)
+        }
+
+        res.render("author_list",{
+            title:"Author List",
+            author_list: list_authors
+        })
+    })
 }
 
 exports.author_detail = (req, res) => {
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose');
+const {DateTime} = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -18,5 +19,9 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual('url').get(function(){
     return `catalog/bookinstance/${this._id}`
 });
+
+BookInstanceSchema.virtual('due_back_formatted').get(function(){
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+})
 
 module.exports = mongoose.model('BookInstance',BookInstanceSchema);
