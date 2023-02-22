abc94c16286049c240943192d808b90e4b1e48e8
mika-alpha <yuukeikioku@gmail.com>
2023-02-21 21:22:24 -0500

    Add Book instance detail view

--- models/author.js
+++ models/author.js
@@ -34,4 +34,9 @@ AuthorSchema.virtual('date_of_death_formatted').get(function(){
 
 })
 
+AuthorSchema.virtual('lifespan').get(function(){
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '' + ' - ' +
+    this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+})
+
 module.exports = mongoose.model("Author", AuthorSchema);
