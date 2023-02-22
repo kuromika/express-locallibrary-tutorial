e805f3cdc1d2b222c69e1b674d7f7fe35a7804cc
mika-alpha <yuukeikioku@gmail.com>
2023-02-21 19:54:18 -0500

    Add Genre details view & fix url virtuals

--- models/book.js
+++ models/book.js
@@ -12,7 +12,7 @@ const BookSchema = new Schema({
 });
 
 BookSchema.virtual("url").get(function(){
-    return `catalog/book/${this._id}`;
+    return `/catalog/book/${this._id}`;
 })
 
 module.exports = mongoose.model("Book", BookSchema);
