4409d5dc60700b368fe35d337d43fa80c31087ca
mika-alpha <yuukeikioku@gmail.com>
2023-02-23 16:00:37 -0500

    Add delete BookInstance post & get

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -102,13 +102,31 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+exports.bookinstance_delete_get = (req, res, next) => {
+  BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec((err, bookinstance) => {
+      if (err) {
+        next(err);
+      }
+      if (bookinstance == null) {
+        res.redirect("/catalog/bookinstances");
+      }
+      res.render("bookinstance_delete", {
+        title: "Delete Instance",
+        instance: bookinstance,
+      });
+    });
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+exports.bookinstance_delete_post = (req, res, next) => {
+  BookInstance.findByIdAndRemove(req.body.instanceid, (err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/catalog/bookinstances");
+  });
 };
 
 // Display BookInstance update form on GET.
