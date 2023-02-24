9773bb8edd3e6c8ff8635310e112151faa6047b3
Kuromika <kuromika.dev@gmail.com>
2023-02-23 22:40:42 -0500

    Add Author Update get post

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -168,10 +168,73 @@ exports.author_delete_post = (req, res, next) => {
   );
 };
 
-exports.author_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+exports.author_update_get = (req, res, next) => {
+  Author.findById(req.params.id).exec((err, result) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (result == null) {
+      const err = new Error("Author not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("author_form", {
+      title: "Update Author",
+      author: result,
+    });
+  });
 };
 
-exports.author_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters"),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invali date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    Author.findByIdAndUpdate(req.params.id, author, {}, (err) => {
+      if (err) {
+        return next;
+      }
+      res.redirect(author.url);
+    });
+  },
+];
